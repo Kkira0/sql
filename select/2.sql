5bee6acaf078a3ea300e9b1c8ea7c7b0aeefc3a9
kira <your@email.ex>
2024-09-06 09:20:40 +0300

    select for certain fields, custom field + distinct

---
+++ select/2.sql
@@ -0,0 +1,16 @@
+-- uzdevums: atlasit datus no tabulas 'customers' 
+-- datubaze 'sql_store'
+-- atlasit vardu un uzvardu un punktus
+-- papilduzd; atlasit papildus kolonu,
+
+-- use sql_store;
+-- select 
+-- first_name , 
+-- last_name, 
+-- points, 
+-- points+10 as "points increased"
+--  from customers ;
+
+select distinct STATE from customers;
+
+
